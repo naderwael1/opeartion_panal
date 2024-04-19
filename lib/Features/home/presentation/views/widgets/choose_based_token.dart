921fb0dea1b2156d1e3a86a3bf09b200f0ad6309
mahmoud <mahmoud.modo@gmail.com>
2024-04-19 14:56:38 +0200

    add storage

--- lib/Features/home/presentation/views/widgets/choose_based_token.dart
+++ lib/Features/home/presentation/views/widgets/choose_based_token.dart
@@ -2,6 +2,7 @@ import 'package:bloc_v2/Features/emp_features/presentation/all_emp_screen.dart';
 import 'package:bloc_v2/Features/home/presentation/views/widgets/edit_upload_product_form.dart';
 import 'package:bloc_v2/add_general_Section/add_general_section.dart';
 import 'package:bloc_v2/add_register/add_register_employee.dart';
+import 'package:bloc_v2/add_storage/add_storage_screen.dart';
 import 'package:bloc_v2/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -36,7 +37,7 @@ class HomeBody extends StatelessWidget {
           _buildCard(context, 'HR Dept', hrImage, const AllEmployeeScreen()),
           _buildCard(context, 'Operation Manager', operationImage, const AllBranchScreen()),
           _buildCard(context, 'Branch Manager', mangerImage, const AllBranchScreen()),
-          _buildCard(context, 'Storage', storgeImage, const AllBranchScreen()),
+          _buildCard(context, 'Storage', storgeImage, const AddStorage()),
           _buildCard(context, 'Upload', cloud, const AddRegisterEmp()),
         ],
       ),
---
+++ lib/add_storage/add_storage_model.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart' as http;
+Future<String> addStorageModel({
+  required String storageName,
+  required String storageAddress,
+  required String managerId,
+}) async {
+  final url = 'http://192.168.56.1:4000/admin/branch/add-storage';
+  try {
+    final response = await http.post(
+      Uri.parse(url),
+      body: {
+        'storageName': storageName,
+        'storageAddress': storageAddress,
+        'managerId': managerId,
+      },
+    );
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      // Success
+      final add_menu_item = response.body; // Assuming the branchId is returned in the response body
+      return add_menu_item;
+    } else {
+      // Failure
+      throw Exception('Failed to Add Storage: ${response.statusCode}');
+    }
+  } catch (e) {
+    throw Exception('Error AddStorage: $e');
+  }
+}
