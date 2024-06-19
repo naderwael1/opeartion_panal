985ec417f086fe80dcd93d54381ea98aab3666c4
mahmoud <mahmoud.modo@gmail.com>
2024-06-20 00:25:13 +0300

    100% Attendance In and out

--- lib/screens/attendance_In_Out.dart
+++ lib/screens/attendance_In_Out.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:bloc_v2/add_register/style.dart';
-import 'package:bloc_v2/screens/employee_vacation_model.dart';
+import 'package:bloc_v2/screens/attendanceOut_model.dart';
+import 'package:bloc_v2/screens/attendance_In.dart';
 import 'package:cherry_toast/cherry_toast.dart';
 import 'package:cherry_toast/resources/arrays.dart';
 import 'package:flutter/material.dart';
@@ -16,13 +17,17 @@ class AttendanceInAndOut extends StatefulWidget {
 
 class _AttendanceInAndOut extends State<AttendanceInAndOut> {
   TextEditingController employeeIddController = TextEditingController();
-  TextEditingController vacationStartDateController = TextEditingController();
-  TextEditingController vacationReasonController = TextEditingController();
-  TextEditingController vacationEndController = TextEditingController();
+  TextEditingController scheduleIdController = TextEditingController();
+  TextEditingController employeeId1Controller = TextEditingController();
+  TextEditingController scheduleId1Controller = TextEditingController();
+  TextEditingController timeInController = TextEditingController();
+  TextEditingController timeOutController = TextEditingController();
 
   int _selectedIndex = 1;
   bool isEditing = false;
   final _formKey = GlobalKey<FormState>();
+  final _formKeyOut = GlobalKey<FormState>();
+
   List<Map<String, dynamic>> activeEmployees = [];
   Timer? _timer;
 
@@ -68,28 +73,56 @@ class _AttendanceInAndOut extends State<AttendanceInAndOut> {
     }
   }
 
-  Future<void> _selectDate(TextEditingController controller) async {
-    DateTime? picked = await showDatePicker(
+  Future<void> _selectDateTime(TextEditingController controller) async {
+    // Pick the date
+    DateTime? pickedDate = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
       firstDate: DateTime(2000),
       lastDate: DateTime(2100),
     );
 
-    if (picked != null) {
-      String formattedDate = picked.toLocal().toString().split(' ')[0];
-      setState(() {
-        controller.text = formattedDate;
-      });
+    if (pickedDate != null) {
+      // Pick the time
+      TimeOfDay? pickedTime = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.now(),
+      );
+
+      if (pickedTime != null) {
+        // Combine the date and time
+        DateTime pickedDateTime = DateTime(
+          pickedDate.year,
+          pickedDate.month,
+          pickedDate.day,
+          pickedTime.hour,
+          pickedTime.minute,
+        );
+
+        // Format the combined date and time
+        String formattedDateTime = pickedDateTime.toLocal().toString();
+
+        // Update the controller's text
+        setState(() {
+          controller.text = formattedDateTime;
+        });
+      }
     }
   }
 
   void clearForm() {
-    vacationStartDateController.clear();
-    vacationReasonController.clear();
-    vacationEndController.clear();
+    timeInController.clear();
+    scheduleIdController.clear();
     setState(() {
       employeeIddController.clear();
+    });
+  }
+
+    void clearFormOut() {
+    timeOutController.clear();
+    scheduleId1Controller.clear();
+    setState(() {
+      employeeId1Controller.clear();
     });
   }
 
@@ -112,7 +145,7 @@ class _AttendanceInAndOut extends State<AttendanceInAndOut> {
                       ),
                       const Center(
                         child: Text(
-                          'Add Employee Vacation',
+                          'Attendance In | Out',
                           style: TextStyle(
                             color: Colors.white,
                             fontSize: 28,
@@ -131,6 +164,19 @@ class _AttendanceInAndOut extends State<AttendanceInAndOut> {
                   key: _formKey,
                   child: Column(
                     children: [
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: scheduleIdController,
+                        decoration: inputDecoration.copyWith(
+                          labelText: 'Schedule Id',
+                        ), // Allows the text field to grow with the content
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a reason for the vacation';
+                          }
+                          return null;
+                        },
+                      ),
                       const SizedBox(height: 20),
                       DropdownButtonFormField<int>(
                         value: employeeIddController.text.isEmpty
@@ -158,36 +204,11 @@ class _AttendanceInAndOut extends State<AttendanceInAndOut> {
                           return null;
                         },
                       ),
-                      const SizedBox(height: 20),
-                      TextFormField(
-                        controller: vacationStartDateController,
-                        decoration: const InputDecoration(
-                          labelText: 'Vacation Start Date',
-                          filled: true,
-                          prefixIcon: Icon(Icons.calendar_today),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blue),
-                          ),
-                        ),
-                        readOnly: true,
-                        onTap: () {
-                          _selectDate(vacationStartDateController);
-                        },
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter a valid date';
-                          }
-                          return null;
-                        },
-                      ),
                       const SizedBox(height: 30),
                       TextFormField(
-                        controller: vacationEndController,
+                        controller: timeInController,
                         decoration: const InputDecoration(
-                          labelText: 'Vacation End Date',
+                          labelText: 'Attendance In',
                           filled: true,
                           prefixIcon: Icon(Icons.calendar_today),
                           enabledBorder: OutlineInputBorder(
@@ -199,27 +220,11 @@ class _AttendanceInAndOut extends State<AttendanceInAndOut> {
                         ),
                         readOnly: true,
                         onTap: () {
-                          _selectDate(vacationEndController);
+                          _selectDateTime(timeInController);
                         },
                         validator: (value) {
                           if (value == null || value.isEmpty) {
                             return 'Please enter a Vacation End Date';
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 20),
-                      TextFormField(
-                        controller: vacationReasonController,
-                        decoration: inputDecoration.copyWith(
-                          labelText: 'Vacation Reason',
-                        ),
-                        maxLength: 150,
-                        maxLines:
-                            null, // Allows the text field to grow with the content
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter a reason for the vacation';
                           }
                           return null;
                         },
@@ -237,24 +242,155 @@ class _AttendanceInAndOut extends State<AttendanceInAndOut> {
                           ElevatedButton.icon(
                             style: elevatedButtonStyle,
                             icon: const Icon(Icons.upload),
-                            label: const Text("Add Vacation Employee"),
+                            label: const Text("Add Attendance In"),
                             onPressed: () async {
                               if (_formKey.currentState!.validate()) {
                                 try {
-                                  final addRegisterEmp = ModelEmployeeVacation(
+                                  final addRegisterEmp = AttendanceIn(
+                                    scheduleId: scheduleIdController.text,
                                     employeeId: employeeIddController.text,
-                                    vacationStartDate:
-                                        vacationStartDateController.text,
-                                    vacationEndDate: vacationEndController.text,
-                                    vacationReason:
-                                        vacationReasonController.text,
+                                    timeIn: timeInController.text,
                                   );
-                                  print('Adding employee: $addRegisterEmp');
+                                  print(
+                                      'Adding Attendance In: $addRegisterEmp');
                                   CherryToast.success(
                                     animationType: AnimationType.fromRight,
                                     toastPosition: Position.bottom,
                                     description: const Text(
-                                      "Employee Schedule successfully",
+                                      "Employee Attendance In successfully",
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                                  ).show(context);
+                                  clearForm();
+                                } catch (e) {
+                                  CherryToast.error(
+                                    toastPosition: Position.bottom,
+                                    animationType: AnimationType.fromRight,
+                                    description: const Text(
+                                      "Something went wrong!",
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                                  ).show(context);
+                                }
+                              } else {
+                                CherryToast.warning(
+                                  toastPosition: Position.bottom,
+                                  animationType: AnimationType.fromLeft,
+                                  description: const Text(
+                                    "Data is not valid or not complete",
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                ).show(context);
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+                child: Form(
+                  key: _formKeyOut,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: scheduleId1Controller,
+                        decoration: inputDecoration.copyWith(
+                          labelText: 'Schedule Id',
+                        ), // Allows the text field to grow with the content
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a reason for the vacation';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      DropdownButtonFormField<int>(
+                        value: employeeId1Controller.text.isEmpty
+                            ? null
+                            : int.tryParse(employeeId1Controller.text),
+                        onChanged: (newValue) {
+                          setState(() {
+                            employeeId1Controller.text =
+                                newValue?.toString() ?? '';
+                          });
+                        },
+                        items: activeEmployees.map((employee) {
+                          return DropdownMenuItem<int>(
+                            value: employee['employee_id'],
+                            child: Text(employee['employee_name']),
+                          );
+                        }).toList(),
+                        decoration: inputDecoration.copyWith(
+                          labelText: 'Employee Name',
+                        ),
+                        validator: (value) {
+                          if (value == null) {
+                            return 'Please select an Employee Name';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 30),
+                      TextFormField(
+                        controller: timeOutController,
+                        decoration: const InputDecoration(
+                          labelText: 'Attendance In',
+                          filled: true,
+                          prefixIcon: Icon(Icons.calendar_today),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
+                        ),
+                        readOnly: true,
+                        onTap: () {
+                          _selectDateTime(timeOutController);
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a Vacation End Date';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ElevatedButton.icon(
+                            style: clearButtonStyle,
+                            icon: const Icon(Icons.clear),
+                            label: const Text("Clear"),
+                            onPressed: clearFormOut,
+                          ),
+                          ElevatedButton.icon(
+                            style: elevatedButtonStyle,
+                            icon: const Icon(Icons.upload),
+                            label: const Text("Add Attendance Out"),
+                            onPressed: () async {
+                              if (_formKeyOut.currentState!.validate()) {
+                                try {
+                                  final addRegisterEmp = EmployeeOUTModel(
+                                    scheduleId: scheduleId1Controller.text,
+                                    employeeId: employeeId1Controller.text,
+                                    timeOut: timeOutController.text,
+                                  );
+                                  print(
+                                      'Adding Attendance Out: $addRegisterEmp');
+                                  CherryToast.success(
+                                    animationType: AnimationType.fromRight,
+                                    toastPosition: Position.bottom,
+                                    description: const Text(
+                                      "Employee Attendance Out successfully",
                                       style: TextStyle(color: Colors.black),
                                     ),
                                   ).show(context);
