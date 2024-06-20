c75e595c55f1fa501b4c53633567e5039d0348a0
mahmoud <mahmoud.modo@gmail.com>
2024-06-21 01:57:59 +0300

    Get position

--- lib/Features/emp_features/presentation/postion_secreen.dart
+++ lib/Features/emp_features/presentation/postion_secreen.dart
@@ -1,6 +1,5 @@
 import 'package:bloc_v2/Features/emp_features/Data/get_all_positions.dart';
 import 'package:bloc_v2/Features/emp_features/models/positon_models.dart';
-import 'package:bloc_v2/Features/emp_features/presentation/custom_clip_path';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -10,30 +9,14 @@ class PositionListScreen extends StatefulWidget {
   _PositionListScreenState createState() => _PositionListScreenState();
 }
 
-class _PositionListScreenState extends State<PositionListScreen>
-    with SingleTickerProviderStateMixin {
+class _PositionListScreenState extends State<PositionListScreen> {
   late Future<List<PositionModel>> futurePositions;
-  late AnimationController _controller;
-  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    )..repeat(reverse: true);
-
-    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
-
-    // Ensure that the future is initialized after setting up the animation
+    // Ensure that the future is initialized
     futurePositions = fetchPositions();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 
   @override
@@ -52,7 +35,6 @@ class _PositionListScreenState extends State<PositionListScreen>
       body: Stack(
         children: [
           ClipPath(
-            clipper: CustomClipPath(),
             child: Container(
               color: Colors.teal.withOpacity(0.2),
               height: 200,
@@ -69,28 +51,25 @@ class _PositionListScreenState extends State<PositionListScreen>
                 return Center(child: Text('No positions found'));
               } else {
                 final positions = snapshot.data!;
-                return FadeTransition(
-                  opacity: _animation,
-                  child: ListView.builder(
-                    itemCount: positions.length,
-                    itemBuilder: (context, index) {
-                      final position = positions[index];
-                      return Card(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                        child: ListTile(
-                          title: Text(
-                            position.jobName,
-                            style: GoogleFonts.lato(
-                              textStyle: TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.w600),
-                            ),
+                return ListView.builder(
+                  itemCount: positions.length,
+                  itemBuilder: (context, index) {
+                    final position = positions[index];
+                    return Card(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      child: ListTile(
+                        title: Text(
+                          position.jobName,
+                          style: GoogleFonts.lato(
+                            textStyle: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w600),
                           ),
-                          subtitle: Text('ID: ${position.positionId}'),
                         ),
-                      );
-                    },
-                  ),
+                        subtitle: Text('ID: ${position.positionId}'),
+                      ),
+                    );
+                  },
                 );
               }
             },
