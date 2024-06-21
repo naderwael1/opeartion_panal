d91f3e9b3cd797e5fd94ddb44e5e5aa5c98f63bd
mahmoud <mahmoud.modo@gmail.com>
2024-06-21 04:45:14 +0300

    Done Status Changed

--- lib/Features/emp_features/presentation/custom_card.dart
+++ lib/Features/emp_features/presentation/custom_card.dart
@@ -1,3 +1,6 @@
+import 'package:bloc_v2/Features/emp_features/presentation/edit_change_status_model.dart';
+import 'package:cherry_toast/cherry_toast.dart';
+import 'package:cherry_toast/resources/arrays.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:bloc_v2/Features/edit_data_employee/edit_data_employee_screen.dart';
@@ -11,7 +14,9 @@ class CustomCard extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
-  String capitalize(String s) => s.isNotEmpty ? '${s[0].toUpperCase()}${s.substring(1).toLowerCase()}' : '';
+  String capitalize(String s) => s.isNotEmpty
+      ? '${s[0].toUpperCase()}${s.substring(1).toLowerCase()}'
+      : '';
 
   @override
   Widget build(BuildContext context) {
@@ -68,14 +73,16 @@ class CustomCard extends StatelessWidget {
                 onPressed: () {
                   // Action for this button
                 },
-                icon: Icon(Icons.remove_red_eye, color: Colors.blueGrey.shade700),
+                icon:
+                    Icon(Icons.remove_red_eye, color: Colors.blueGrey.shade700),
               ),
               IconButton(
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => EditEmployeeScreen(employeeId: activeEmployee.employeeId),
+                      builder: (context) => EditEmployeeScreen(
+                          employeeId: activeEmployee.employeeId),
                     ),
                   );
                 },
@@ -83,7 +90,8 @@ class CustomCard extends StatelessWidget {
               ),
               IconButton(
                 onPressed: () {
-                  // Action for this button
+                  _showDeleteDialog(
+                      context, activeEmployee.employeeId.toString());
                 },
                 icon: Icon(Icons.delete, color: Colors.blueGrey.shade700),
               ),
@@ -93,4 +101,118 @@ class CustomCard extends StatelessWidget {
       ),
     );
   }
+
+  void _showDeleteDialog(BuildContext context, String employeeId) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        title: Text(
+          'Select Action',
+          style: GoogleFonts.roboto(
+            textStyle: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.blueGrey.shade900,
+            ),
+          ),
+        ),
+        content: Text(
+          'What do you want to do with this employee?',
+          style: GoogleFonts.roboto(
+            textStyle: TextStyle(
+              fontSize: 16,
+              color: Colors.blueGrey.shade800,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.blueGrey.shade100,
+        actions: <Widget>[
+          TextButton(
+            onPressed: () async {
+              try {
+                final addStorage_Model = await editChangeStatusModel(
+                  employeeId: employeeId,
+                  employeeStatus: 'inactive',
+                );
+                print('Adding Storage: $addStorage_Model');
+                CherryToast.success(
+                  animationType: AnimationType.fromRight,
+                  toastPosition: Position.bottom,
+                  description: const Text(
+                    "Edit Status successfully",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ).show(context);
+                Navigator.of(context).pop();
+              } catch (e) {
+                print('Error add menu item: $e');
+                CherryToast.error(
+                  toastPosition: Position.bottom,
+                  animationType: AnimationType.fromRight,
+                  description: const Text(
+                    "Something went wrong!",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ).show(context);
+              }
+            },
+            child: Text(
+              'Inactive',
+              style: TextStyle(color: Colors.blueGrey.shade700),
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              try {
+                final addStorage_Model = await editChangeStatusModel(
+                  employeeId: employeeId,
+                  employeeStatus: 'pending',
+                );
+                print('Adding Storage: $addStorage_Model');
+                CherryToast.success(
+                  animationType: AnimationType.fromRight,
+                  toastPosition: Position.bottom,
+                  description: const Text(
+                    "Edit Status successfully",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ).show(context);
+                Navigator.of(context).pop();
+              } catch (e) {
+                print('Error add menu item: $e');
+                CherryToast.error(
+                  toastPosition: Position.bottom,
+                  animationType: AnimationType.fromRight,
+                  description: const Text(
+                    "Something went wrong!",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ).show(context);
+              }
+            },
+            child: Text(
+              'Pending',
+              style: TextStyle(color: Colors.blueGrey.shade700),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              // Action for Canceled
+            },
+            child: Text(
+              'Canceled',
+              style: TextStyle(color: Colors.blueGrey.shade700),
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
+
 }
---
+++ lib/Features/emp_features/presentation/edit_change_status_model.dart
@@ -0,0 +1,31 @@
+import 'package:http/http.dart' as http;
+
+Future<String> editChangeStatusModel({
+  required String employeeId,
+  required String employeeStatus,
+}) async {
+  const url = 'http://192.168.56.1:4000/admin/employees/employeeStatusChange';
+  try {
+    final response = await http.post(
+      Uri.parse(url),
+      body: {
+        'employeeId': employeeId,
+        'employeeStatus': employeeStatus,
+      },
+    );
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      final addMenuItem = response.body; // Assuming the response body contains the data you need
+      print('Status: ${response.statusCode}');
+      return addMenuItem;
+    } else {
+      print('Status: ${response.statusCode}');
+      throw Exception('Failed to edit Status: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error: $e');
+    throw Exception('Error edit Status: $e');
+  }
+}
+
+
+
