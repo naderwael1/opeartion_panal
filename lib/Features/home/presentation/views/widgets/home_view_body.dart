1cf46b36adcd5fec0e95725fcf105197fd71c07b
mahmoud <mahmoud.modo@gmail.com>
2024-06-27 00:49:19 +0300

    Clear code to Start implement logic login form API

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,9 +1,5 @@
-import 'package:bloc_v2/Features/home/presentation/views/widgets/login_view.dart';
 import 'package:bloc_v2/Features/home/presentation/views/widgets/new_login_screen.dart';
-import 'package:bloc_v2/core/utils/styles.dart';
 import 'package:flutter/material.dart';
-
-import 'login_screen.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({Key? key}) : super(key: key);
--- lib/Features/home/presentation/views/widgets/new_login_screen.dart
+++ lib/Features/home/presentation/views/widgets/new_login_screen.dart
@@ -1,8 +1,16 @@
 import 'package:bloc_v2/Features/home/presentation/views/widgets/choose_based_token.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreenNew extends StatelessWidget {
+class LoginScreenNew extends StatefulWidget {
   const LoginScreenNew({super.key});
+
+  @override
+  _LoginScreenNewState createState() => _LoginScreenNewState();
+}
+
+class _LoginScreenNewState extends State<LoginScreenNew> {
+  bool _obscureText = true; // Initial value for obscuring text
+  bool _rememberMe = false; // Initial value for Remember Me checkbox
 
   @override
   Widget build(BuildContext context) {
@@ -10,128 +18,131 @@ class LoginScreenNew extends StatelessWidget {
     double topPadding = MediaQuery.of(context).padding.top;
 
     // Define color constants for better theme management
-    const Color primaryColor =
-        Color.fromARGB(255, 66, 84, 147); // A soothing purple
-    const Color secondaryColor =
-        Color(0xFFf4f4f4); // Light grey for backgrounds
+    const Color primaryColor = Color.fromARGB(255, 66, 84, 147); // A soothing purple
+    const Color secondaryColor = Color(0xFFf4f4f4); // Light grey for backgrounds
     const Color accentColor = Color(0xFF4caf50); // A fresh green for accents
 
     return SafeArea(
       child: Scaffold(
         backgroundColor: secondaryColor, // Use secondary color for background
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              constraints: BoxConstraints(minHeight: screenHeight - topPadding),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    'Welcome 👋',
-                    style: TextStyle(
-                      fontSize: 32.0,
-                      fontWeight: FontWeight.bold,
-                      color: primaryColor, // Use primary color for headings
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            constraints: BoxConstraints(minHeight: screenHeight - topPadding),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Welcome 👋',
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.bold,
+                    color: primaryColor, // Use primary color for headings
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                const SizedBox(height: 10.0),
+                Text(
+                  'Please enter your account details provided by HR',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.grey[600], // Use a dark grey for subtitles
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'example@kamon.com',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0), // More rounded borders
+                      borderSide: BorderSide(color: primaryColor),
                     ),
-                    textAlign: TextAlign.left,
-                  ),
-                  const SizedBox(height: 10.0),
-                  Text(
-                    'Please enter your account details provided by HR',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.grey[600], // Use a dark grey for subtitles
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      borderSide: BorderSide(color: primaryColor, width: 2.0),
                     ),
-                    textAlign: TextAlign.left,
-                  ),
-                  const SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email Address',
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.circular(25.0), // More rounded borders
-                        borderSide: BorderSide(color: primaryColor),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: BorderSide(color: primaryColor, width: 2.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: BorderSide(color: primaryColor, width: 2.5),
-                      ),
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  const SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.circular(25.0), // More rounded borders
-                        borderSide: BorderSide(color: primaryColor),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: BorderSide(color: primaryColor, width: 2.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: BorderSide(color: primaryColor, width: 2.5),
-                      ),
-                      suffixIcon: const Icon(Icons.visibility_off),
-                    ),
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 8.0),
-                  Row(
-                    children: [
-                      Checkbox(
-                        value: false,
-                        onChanged: (bool? newValue) {},
-                        activeColor: accentColor,
-                      ),
-                      const Text('Remember Me'),
-                      const Spacer(),
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text('Forgot Password?'),
-                        style: TextButton.styleFrom(
-                          foregroundColor: primaryColor,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => HomeBody()),
-                      );
-                    },
-                    child: const Text('Login'),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: primaryColor,
-                      foregroundColor:
-                          Colors.white, // Use white color for button text
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      minimumSize:
-                          const Size(double.infinity, 50), // Full width button
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      borderSide: BorderSide(color: primaryColor, width: 2.5),
                     ),
                   ),
-                ],
-              ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0), // More rounded borders
+                      borderSide: BorderSide(color: primaryColor),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      borderSide: BorderSide(color: primaryColor, width: 2.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      borderSide: BorderSide(color: primaryColor, width: 2.5),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(_obscureText ? Icons.visibility_off : Icons.visibility),
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                    ),
+                  ),
+                  obscureText: _obscureText,
+                ),
+                const SizedBox(height: 8.0),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: _rememberMe,
+                      onChanged: (bool? newValue) {
+                        setState(() {
+                          _rememberMe = newValue ?? false;
+                        });
+                      },
+                      activeColor: accentColor,
+                    ),
+                    const Text('Remember Me'),
+                    const Spacer(),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Forgot Password?'),
+                      style: TextButton.styleFrom(
+                        foregroundColor: primaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomeBody()),
+                    );
+                  },
+                  child: const Text('Login'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    foregroundColor: Colors.white, // Use white color for button text
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    minimumSize: const Size(double.infinity, 50), // Full width button
+                  ),
+                ),
+              ],
             ),
           ),
         ),
